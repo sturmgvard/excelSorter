58a70992dcca926953cd62d4624df07b0b8b6723
boza <sedof@inbox.ru>
2019-06-22 12:30:52 +0300

    commit 2 day 1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,15 +5,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main extends Application {
 
+    public static ArrayList<Workbook> wbList = new ArrayList();
+    public static ArrayList<Row> rowsList = new ArrayList();
+
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -26,11 +34,38 @@ public class Main extends Application {
     }
 
 
-
-
-    public String readFileName() throws IOException {
+    public static String readFileName() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         return bufferedReader.readLine();
     }
+
+    public static void addWorkbooks(int count) throws IOException {
+        for (int i = 0; i < count; i++) {
+            try (FileInputStream fis = new FileInputStream(readFileName())) {
+                wbList.add(new XSSFWorkbook(fis));
+            }
+        }
+    }
+
+    public static void fillRowsList(String search) {
+        if (!wbList.isEmpty()) {
+            for (Workbook workbook : wbList) {
+                for (Sheet sheet : workbook) {
+                    for (Row row : sheet) {
+                        for (Cell cell : row) {
+                            if(cell != null){
+                                if(cell.getCellType() == CellType.STRING){
+                                    if(cell.toString().toLowerCase().contains(search.toLowerCase())){
+                                        rowsList.add(row);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 
 }
